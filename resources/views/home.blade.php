a3225ba30875dece11ad0ecae5be262722310777
manuelgaleone <galeonemanuel14@gmail.com>
2022-12-20 15:32:44 +0100

    Fixed contacts, working on seeds

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="main_content_wrap y-3">
-    <div class="main_content_elements">
-        <div class="jumbotron">
-            <h1 class="display-4">TrainCheck 24</h1>
-            <p class="lead">Il portale di ogni Viaggiatore</p>
-            <hr class="my-4">
-            <p>Controlla in maniera costante ed aggiornata l'andamento dei treni in tutt'Italia.</p>
-            <p class="lead">
-                <a class="btn btn-primary btn-lg" href="{{route('trains')}}" role="button">Scopri ora</a>
-            </p>
+<div class="main_content_wrap">
+    <div class="main_content_elements py-2">
+        <div class="container">
+            <div class="jumbotron">
+                <h1 class="display-4">TrainCheck 24</h1>
+                <p class="lead">Il portale di ogni Viaggiatore</p>
+                <hr class="my-4">
+                <p>Controlla in maniera costante ed aggiornata l'andamento dei treni in tutt'Italia.</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="{{route('trains')}}" role="button">Scopri ora</a>
+                </p>
+            </div>
         </div>
     </div>
 </div>
